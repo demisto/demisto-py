f34a8eb579f1dc8bdebb941292fd470bf70d9189
slavikm <slavik.markovich@gmail.com>
2017-04-21 15:50:43 -0700

    An example of retrieving integrations and their commands

---
+++ integrations_example.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python2.7
+
+# example
+# An example to retrieve all availabile integrations and commands for each
+# Works on Demisto 2.5 onwards
+#
+# Author:       Slavik Markovich
+# Version:      1.0
+#
+
+import json
+import argparse
+import csv
+import demisto
+
+def p(what):
+    if verbose:
+        print(what)
+
+def options_handler():
+    parser = argparse.ArgumentParser(description='Integrations and commands')
+    parser.add_argument('-u', '--user', help='The username for the login', required=True)
+    parser.add_argument('-p', '--password', help='The password for the login', required=True)
+    parser.add_argument('-s', '--server', help='The server URL to connect to', required=True)
+    parser.add_argument('-q', '--quiet', action='store_false', dest='verbose', default=True, help="no extra prints")
+    parser.add_argument('-o', '--output', help='The output CSV file', default='integrations.csv')
+    options = parser.parse_args()
+    global verbose
+    verbose = options.verbose
+    return options
+
+def main():
+    options = options_handler()
+    c = demisto.DemistoClient(options.user, options.password, options.server)
+    c.Login()
+    integrationsResponse = c.req('GET', 'settings/integration-commands', None)
+    if integrationsResponse.status_code != 200:
+        raise RuntimeError('Error getting integration data - %d (%s)' % (integrationsResponse.status_code, integrationsResponse.reason))
+    integrations = sorted(integrationsResponse.json(), key=lambda m: m['category'].lower() + '---' + m['name'].lower())
+    with open(options.output, 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=['Category', 'Product', 'ProductDescription', 'Command', 'CommandDescription'])
+        writer.writeheader()
+        for m in integrations:
+            if 'commands' in m:
+                for cmd in m['commands']:
+                    writer.writerow({'Category': m['category'], 'Product': m['name'], 'ProductDescription': m['description'].encode('ascii', 'ignore'),
+                        'Command': cmd['name'], 'CommandDescription': cmd['description'].encode('ascii', 'ignore')})
+            else:
+                writer.writerow({'Category': m['category'], 'Product': m['name'], 'ProductDescription': m['description'].encode('ascii', 'ignore')})
+
+if __name__ == '__main__':
+    main()

9ad8bba98cd0966e0bfb10e42c5c52dbe978f850
omitroom13@gmail.com <omitroom13@gmail.com>
2018-11-17 17:28:37 +0900

    To run on Python3, modified by command : 2to3 -w -n *.py

--- batch_util_example.py
+++ batch_util_example.py
@@ -42,9 +42,9 @@ def main():
     options = options_handler()
     c = demisto.DemistoClient(options.key, options.server)
     incidents = c.SearchIncidents(options.page, 0, options.filter)
-    print('using filter %s' %  options.filter)
-    print('Total #incidents: %d, incidents going to be updated' % (incidents['total']))
-    proceed = raw_input('OK to proceed (type y, yes or leave empty)? ')
+    print(('using filter %s' %  options.filter))
+    print(('Total #incidents: %d, incidents going to be updated' % (incidents['total'])))
+    proceed = input('OK to proceed (type y, yes or leave empty)? ')
     proceed = proceed.lower()
     if proceed == 'y' or proceed == 'yes' or proceed == '':
         if options.action == 'close':
@@ -57,9 +57,9 @@ def main():
                 raise RuntimeError('Error updating incidents - %d (%s)' % (r.status_code, r.reason))
             rj = json.loads(r.content)
             if rj['notUpdated'] > 0:
-                print('Updated %d and could not update %d' % (rj['total']), rj['notUpdated'])
+                print(('Updated %d and could not update %d' % (rj['total']), rj['notUpdated']))
             else:
-                print('Updated %d incidents' % rj['total'])
+                print(('Updated %d incidents' % rj['total']))
 
 if __name__ == '__main__':
     main()

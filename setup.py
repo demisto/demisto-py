4d073b93e776ac85fdc7c573114c417eda0e2b9d
Andrew Shamah <andrew@demisto.com>
2019-08-05 15:48:22 +0300

    Init Commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='demisto-py',
+    version='0.1',
+    packages=['demisto'],
+    url='https://github.com/demisto/demisto-py',
+    license='Apache License 2.0',
+    author='Demisto',
+    author_email='support@demisto.com',
+    description='A Python library for the Demisto API.'
+)

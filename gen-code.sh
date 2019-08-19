7671655bd95b926498f8702f2e0af39432a4284d
Guy Lichtman <1395797+glicht@users.noreply.github.com>
2019-08-19 18:17:03 +0300

    gen swagger code fixes

--- gen-code.sh
+++ gen-code.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+
 rm -r demisto_client/demisto_api
-rm -r demisto_client/docs
+rm -r docs
 mkdir demisto_client
-docker run --rm -it -v `pwd`:/work -w /work swaggerapi/swagger-codegen-cli generate -i /work/server_api_swagger.json -l python -o /work/demisto_client -c /work/swagger-config.json
-rm -r demisto_client/test
+mv README.md README.md.org
+docker run --rm -it -v `pwd`:/work -w /work swaggerapi/swagger-codegen-cli generate -i /work/server_api_swagger.json -l python -o /work -c /work/swagger-config.json
+rm -r test
+mv demisto_client.demisto_api/*.py demisto_client/demisto_api/.
+mv demisto_client.demisto_api/api/*.py demisto_client/demisto_api/api/.
+mv demisto_client.demisto_api/models/*.py demisto_client/demisto_api/models/.
+rmdir demisto_client.demisto_api/api
+rmdir demisto_client.demisto_api/models
+rmdir demisto_client.demisto_api
+mv README.md docs/.
+# TODO: need to fix back links to README.md
+mv README.md.org README.md

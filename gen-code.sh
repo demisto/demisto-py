b1271937d73cebb4714ff88533f21fe77ac0f78b
Guy Lichtman <1395797+glicht@users.noreply.github.com>
2019-08-19 18:52:21 +0300

    add fixes for imports

--- gen-code.sh
+++ gen-code.sh
@@ -4,7 +4,7 @@ rm -r demisto_client/demisto_api
 rm -r docs
 mkdir demisto_client
 mv README.md README.md.org
-docker run --rm -it -v `pwd`:/work -w /work swaggerapi/swagger-codegen-cli generate -i /work/server_api_swagger.json -l python -o /work -c /work/swagger-config.json
+docker run --rm -it -v `pwd`:/work -w /work swaggerapi/swagger-codegen-cli:2.4.7 generate -i /work/server_api_swagger.json -l python -o /work -c /work/swagger-config.json
 rm -r test
 mv demisto_client.demisto_api/*.py demisto_client/demisto_api/.
 mv demisto_client.demisto_api/api/*.py demisto_client/demisto_api/api/.
@@ -15,3 +15,7 @@ rmdir demisto_client.demisto_api
 mv README.md docs/.
 # TODO: need to fix back links to README.md
 mv README.md.org README.md
+# Not clear why python fails on these imports
+sed -i -e 's/^from demisto_client.demisto_api.models.advance_arg import AdvanceArg/# &/' demisto_client/demisto_api/models/operator_argument.py
+sed -i -e 's/^from demisto_client.demisto_api.models.group import Group/# &/' demisto_client/demisto_api/models/groups.py
+sed -i -e 's/^from demisto_client.demisto_api.models.investigation_playbook import InvestigationPlaybook/# &/' demisto_client/demisto_api/models/investigation_playbook_task.py

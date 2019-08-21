63afe410241037a3891d253139634a92b189b4fe
Andrew Shamah <andrew@demisto.com>
2019-08-21 12:39:12 +0300

    Change to config.json Addition of Configure wrapper

--- demisto_client/__init__.py
+++ demisto_client/__init__.py
@@ -1,5 +1,18 @@
 import demisto_client.demisto_api as demisto_api
 
+# import ApiClient
+from demisto_client.demisto_api import ApiClient
+from demisto_client.demisto_api.configuration import Configuration
 
-def configure():
-    return demisto_api.DefaultApi()
+
+def configure(hostname, api_key, username=None, password=None, verify_ssl=True, proxy=None, debug=False):
+    configuration = Configuration()
+    configuration.api_key['Authorization'] = api_key
+    configuration.host = hostname
+    configuration.username = username
+    configuration.password = password
+    configuration.verify_ssl = verify_ssl
+    configuration.proxy = proxy
+    configuration.debug = debug
+    api_instance = demisto_api.DefaultApi(ApiClient(configuration))
+    return api_instance

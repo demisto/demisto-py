e3982f0e57a0a828d70a9db1da0616b2b4808b3f
Guy Lichtman <1395797+glicht@users.noreply.github.com>
2019-10-01 20:50:08 +0300

    fix search example

--- examples/incidents_search_example.py
+++ examples/incidents_search_example.py
@@ -3,16 +3,16 @@ import demisto_client.demisto_api
 from demisto_client.demisto_api.rest import ApiException
 from pprint import pprint
 
-api_key = 'YOUR API KEY'
-base_url = 'YOUR DEMISTO URL'
+api_key = None  # set to your 'YOUR_API_KEY' or set environment variable: DEMISTO_API_KEY
+base_url = None  # set to your 'http://DEMISTO_HOST' or set environment variable: DEMISTO_BASE_URL
 
 # create an instance of the API class
 api_instance = demisto_client.configure(base_url=base_url, api_key=api_key, debug=True)
 filter = demisto_client.demisto_api.SearchIncidentsData()
 
 # Create incident filter object
-inc_filter = demisto_client.demisto_api.IncidentFilter
-inc_filter.name = 'Test Incident'
+inc_filter = demisto_client.demisto_api.IncidentFilter()
+inc_filter.name = ['test']
 
 filter.filter = inc_filter
 

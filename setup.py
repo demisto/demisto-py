113c0fabbe74a7c199c1eae7d8d60d2099148d10
Guy Lichtman <1395797+glicht@users.noreply.github.com>
2019-10-10 10:21:05 +0300

    setup.py for installing via pip

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='demisto',
+      version='1.0',
+      description='Demisto Client for python',
+      author='Slavik Markovich',
+      url='https://github.com/demisto/demisto-py',
+      packages=['demisto'],
+      )

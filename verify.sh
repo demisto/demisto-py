9c1bbd603ab4406ca633262c7bcc0ebddffe2743
Guy Lichtman <1395797+glicht@users.noreply.github.com>
2020-10-05 16:49:10 +0300

    add verify.sh and check that gen-code does not generate new code

---
+++ verify.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# exit on errors
+set -e
+
+# Script to run extra verifications after tox build completes
+
+# Verify that code-gen.sh doesn't generate a diff
+./gen-code.sh 
+DIFF_OUT=$(git diff --stat)
+
+echo "========================================="
+echo ""
+
+if [[ -n "$DIFF_OUT" ]]; then
+    echo "./gen-code.sh modified the source code. gen-code.sh should not modify the source code. If needed, run gen-code.sh before committing."
+    echo "git diff --stat output:"
+    echo "$DIFF_OUT"
+    exit 1
+fi
+
+echo "All is good"

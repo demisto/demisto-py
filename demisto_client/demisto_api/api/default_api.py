4a749d51c2e52a6c1a1bb930664eea8171a79a71
sabaghadar <hsabag@paloaltonetworks.com>
2021-06-03 20:23:58 +0300

    generate command with swagger

--- demisto_client/demisto_api/api/default_api.py
+++ demisto_client/demisto_api/api/default_api.py
@@ -4711,105 +4711,6 @@ class DefaultApi(object):
             _preload_content=params.get('_preload_content', True),
             _request_timeout=params.get('_request_timeout'),
             collection_formats=collection_formats)
-    
-    def import_report(self, file, **kwargs):  # noqa: E501
-        """Import a report  # noqa: E501
-
-        Import a report to the system, ignoring ID or version, used to import new reports.  # noqa: E501
-        This method makes a synchronous HTTP request by default. To make an
-        asynchronous HTTP request, please pass async_req=True
-        >>> thread = api.import_report(file, async_req=True)
-        >>> result = thread.get()
-
-        :param async_req bool
-        :param file file: file (required)
-        :return: report
-                 If the method is called asynchronously,
-                 returns the request thread.
-        """
-        kwargs['_return_http_data_only'] = True
-        if kwargs.get('async_req'):
-            return self.import_report_with_http_info(file, **kwargs)  # noqa: E501
-        else:
-            (data) = self.import_report_with_http_info(file, **kwargs)  # noqa: E501
-            return data
-
-    def import_report_with_http_info(self, file, **kwargs):  # noqa: E501
-        """Import a report  # noqa: E501
-
-        Import a report to the system, ignoring ID or version, used to import new reports.  # noqa: E501
-        This method makes a synchronous HTTP request by default. To make an
-        asynchronous HTTP request, please pass async_req=True
-        >>> thread = api.import_report_with_http_info(file, async_req=True)
-        >>> result = thread.get()
-
-        :param async_req bool
-        :param file file: file (required)
-        :return: report
-                 If the method is called asynchronously,
-                 returns the request thread.
-        """
-
-        all_params = ['file']  # noqa: E501
-        all_params.append('async_req')
-        all_params.append('_return_http_data_only')
-        all_params.append('_preload_content')
-        all_params.append('_request_timeout')
-
-        params = locals()
-        for key, val in six.iteritems(params['kwargs']):
-            if key not in all_params:
-                raise TypeError(
-                    "Got an unexpected keyword argument '%s'"
-                    " to method import_report" % key
-                )
-            params[key] = val
-        del params['kwargs']
-        # verify the required parameter 'file' is set
-        if ('file' not in params or
-                params['file'] is None):
-            raise ValueError("Missing the required parameter `file` when calling `import_report`")  # noqa: E501
-
-        collection_formats = {}
-
-        path_params = {}
-
-        query_params = []
-
-        header_params = {}
-
-        form_params = []
-        local_var_files = {}
-        if 'file' in params:
-            local_var_files['file'] = params['file']  # noqa: E501
-
-        body_params = None
-        # HTTP header `Accept`
-        header_params['Accept'] = self.api_client.select_header_accept(
-            ['application/json'])  # noqa: E501
-
-        # HTTP header `Content-Type`
-        header_params['Content-Type'] = self.api_client.select_header_content_type(  # noqa: E501
-            ['multipart/form-data'])  # noqa: E501
-
-        # Authentication setting
-        auth_settings = ['api_key', 'csrf_token']  # noqa: E501
-
-        return self.api_client.call_api(
-            '/reports/upload', 'POST',
-            path_params,
-            query_params,
-            header_params,
-            body=body_params,
-            post_params=form_params,
-            files=local_var_files,
-            response_type='report',  # noqa: E501
-            auth_settings=auth_settings,
-            async_req=params.get('async_req'),
-            _return_http_data_only=params.get('_return_http_data_only'),
-            _preload_content=params.get('_preload_content', True),
-            _request_timeout=params.get('_request_timeout'),
-            collection_formats=collection_formats)
 
     def incident_file_upload(self, id, file, **kwargs):  # noqa: E501
         """incident_file_upload  # noqa: E501
@@ -7255,6 +7156,105 @@ class DefaultApi(object):
             post_params=form_params,
             files=local_var_files,
             response_type='Entry',  # noqa: E501
+            auth_settings=auth_settings,
+            async_req=params.get('async_req'),
+            _return_http_data_only=params.get('_return_http_data_only'),
+            _preload_content=params.get('_preload_content', True),
+            _request_timeout=params.get('_request_timeout'),
+            collection_formats=collection_formats)
+
+    def upload_report(self, file, **kwargs):  # noqa: E501
+        """Upload report file to Demisto  # noqa: E501
+
+        Upload report file to Demisto  # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.upload_report(file, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param file file: file (required)
+        :return: list[Report]
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        kwargs['_return_http_data_only'] = True
+        if kwargs.get('async_req'):
+            return self.upload_report_with_http_info(file, **kwargs)  # noqa: E501
+        else:
+            (data) = self.upload_report_with_http_info(file, **kwargs)  # noqa: E501
+            return data
+
+    def upload_report_with_http_info(self, file, **kwargs):  # noqa: E501
+        """Upload report file to Demisto  # noqa: E501
+
+        Upload report file to Demisto  # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.upload_report_with_http_info(file, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param file file: file (required)
+        :return: list[Report]
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+
+        all_params = ['file']  # noqa: E501
+        all_params.append('async_req')
+        all_params.append('_return_http_data_only')
+        all_params.append('_preload_content')
+        all_params.append('_request_timeout')
+
+        params = locals()
+        for key, val in six.iteritems(params['kwargs']):
+            if key not in all_params:
+                raise TypeError(
+                    "Got an unexpected keyword argument '%s'"
+                    " to method upload_report" % key
+                )
+            params[key] = val
+        del params['kwargs']
+        # verify the required parameter 'file' is set
+        if ('file' not in params or
+                params['file'] is None):
+            raise ValueError("Missing the required parameter `file` when calling `upload_report`")  # noqa: E501
+
+        collection_formats = {}
+
+        path_params = {}
+
+        query_params = []
+
+        header_params = {}
+
+        form_params = []
+        local_var_files = {}
+        if 'file' in params:
+            local_var_files['file'] = params['file']  # noqa: E501
+
+        body_params = None
+        # HTTP header `Accept`
+        header_params['Accept'] = self.api_client.select_header_accept(
+            ['multipart/form-data'])  # noqa: E501
+
+        # HTTP header `Content-Type`
+        header_params['Content-Type'] = self.api_client.select_header_content_type(  # noqa: E501
+            ['application/json', 'application/xml'])  # noqa: E501
+
+        # Authentication setting
+        auth_settings = ['api_key', 'csrf_token']  # noqa: E501
+
+        return self.api_client.call_api(
+            '/reports/upload', 'POST',
+            path_params,
+            query_params,
+            header_params,
+            body=body_params,
+            post_params=form_params,
+            files=local_var_files,
+            response_type='list[Report]',  # noqa: E501
             auth_settings=auth_settings,
             async_req=params.get('async_req'),
             _return_http_data_only=params.get('_return_http_data_only'),

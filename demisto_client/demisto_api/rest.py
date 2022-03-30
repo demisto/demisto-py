cd4fae380ed4fab67296609012f9efb882c7ed1d
meichlerpanw <meichler@paloaltonetworks.com>
2022-03-30 08:15:31 +0300

    fix ut

--- demisto_client/demisto_api/rest.py
+++ demisto_client/demisto_api/rest.py
@@ -220,7 +220,10 @@ class RESTClientObject(object):
             # In the python 3, the response.data is bytes.
             # we need to decode it to string.
             if six.PY3 and r.getheader("Content-Type") != "application/octet-stream":
-                r.data = str(r.data)
+                try:
+                    r.data = r.data.decode('utf8')
+                except UnicodeEncodeError:
+                    pass
 
             # log response body
             logger.debug("response body: %s", r.data)

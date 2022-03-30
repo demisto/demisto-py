70990a91dcccb47a0cd20ddc45a50f66f78411ed
meichlerpanw <meichler@paloaltonetworks.com>
2022-03-30 08:51:18 +0300

    run swagger command

--- demisto_client/demisto_api/rest.py
+++ demisto_client/demisto_api/rest.py
@@ -220,10 +220,7 @@ class RESTClientObject(object):
             # In the python 3, the response.data is bytes.
             # we need to decode it to string.
             if six.PY3 and r.getheader("Content-Type") != "application/octet-stream":
-                try:
-                    r.data = r.data.decode("utf8")
-                except UnicodeDecodeError:
-                    pass
+                r.data = r.data.decode('utf8')
 
             # log response body
             logger.debug("response body: %s", r.data)

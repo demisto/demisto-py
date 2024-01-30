634dc749f0225ac83b4c6e8a9bca72b596bd65fb
Michael Yochpaz <8832013+MichaelYochpaz@users.noreply.github.com>
2024-01-30 11:48:01 +0200

    Remove (unused) flake8 dependency

--- demisto_client/demisto_api/__init__.py
+++ demisto_client/demisto_api/__init__.py
@@ -1,7 +1,5 @@
 # coding: utf-8
 
-# flake8: noqa
-
 """
     Demisto API
 
--- demisto_client/demisto_api/api/__init__.py
+++ demisto_client/demisto_api/api/__init__.py
@@ -1,6 +1,4 @@
 from __future__ import absolute_import
 
-# flake8: noqa
-
 # import apis into api package
 from demisto_client.demisto_api.api.default_api import DefaultApi

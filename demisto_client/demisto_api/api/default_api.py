38d2cb3606e282baf1993c23af223b82bcd2ff2c
merit-maita <meretmaayta@gmail.com>
2025-02-24 12:52:11 +0200

    revert

--- demisto_client/demisto_api/api/default_api.py
+++ demisto_client/demisto_api/api/default_api.py
@@ -4923,7 +4923,7 @@ class DefaultApi(object):
         auth_settings = ['api_key', 'csrf_token', 'x-xdr-auth-id']  # noqa: E501
 
         return self.api_client.call_api(
-            '/entry/upload/{id}', 'POST',
+            '/incident/upload/{id}', 'POST',
             path_params,
             query_params,
             header_params,
